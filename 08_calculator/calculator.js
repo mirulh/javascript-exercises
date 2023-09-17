c48b842118be79ff65f07a1aa567f30ec43e8427
mirulh <amirulhaqem122@gmail.com.com>
2023-09-17 20:39:53 +0800

    completed exercises 08 - 12
    it was really hard, need to revise back some other day

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,45 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((prev, next) => prev * next); //don't set initial with 0 cuz anything multiply 0 will be 0
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
+
+// alternative
+
+// const factorial = function (n) {
+//   let answer = 1;
+//   if (n == 0 || n == 1) {
+//     return answer;
+//   } else if (n > 1) {
+//     for (let i = n; i > 0; i--) {
+//       answer = answer * i;
+//     }
+//     return answer;
+//   } else {
+//     return "number has to be positive";
+//   }
+// };
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +48,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
